108efdfa2ca9ef3c116355c48bca93baadab3d19
Thoy <sonsensi@rocketmail.com>
2026-04-07 10:04:53 +0700

    Fix filtering value cleared but ComboBoxItem not show up

--- AvaloniaEx/LookupEdit.cs
+++ AvaloniaEx/LookupEdit.cs
@@ -7,18 +7,25 @@ using Avalonia.Controls.Primitives;
 
 namespace AvaloniaEx.Controls;
 
+/// <summary> ComboBox with simple filtering. Great use on situation with too many selection choice </summary>
+/// <remarks>
+/// By default, <see cref="FilterVisible"/> and <see cref="ClearButtonVisible"/> is enable.<para/>
+/// When popup close, filtering value reset to empty. 
+/// </remarks>
 public class LookupEdit : ComboBox
 {
     protected override System.Type StyleKeyOverride => typeof(ComboBox);
     private readonly TextBox searchBox = new TextBox() { Watermark = "Search..." };
     private Popup? _popup;
     public string? FilterValue => this.searchBox.Text;
+    /// <summary> Set filter textbox on the top of popup visible. </summary>
     public bool FilterVisible
     {
         get => this.searchBox.IsVisible;
         set => this.searchBox.IsVisible = value;
     }
 
+    /// <summary> Button X in bottom of popup selection </summary>
     public bool ClearButtonVisible { get; set; } = true;
 
     public LookupEdit()
@@ -31,7 +38,7 @@ public class LookupEdit : ComboBox
         base.OnApplyTemplate(e);
 
         _popup = e.NameScope.Get<Popup>("PART_Popup");
-        _popup.Closed += (sender, eArgs) => this.searchBox.Clear();
+        _popup.Closed += this.ResetAllItemVisible;
         Border border = e.NameScope.Get<Border>("PopupBorder");
         Control? ctrl = border.Child;
         System.Diagnostics.Debug.Assert(ctrl != null);
@@ -39,7 +46,7 @@ public class LookupEdit : ComboBox
         border.Child = mainPopupPanel;
         mainPopupPanel.Children.Add(searchBox);
         mainPopupPanel.Children.Add(ctrl);
-        if(ClearButtonVisible)
+        if (ClearButtonVisible)
         {
             Button clearButton = new Button()
             {
@@ -51,6 +58,7 @@ public class LookupEdit : ComboBox
             mainPopupPanel.Children.Add(clearButton);
         }
     }
+
 
     private void OnSearchBoxTextChanged(object? sender, TextChangedEventArgs e)
     {
@@ -73,8 +81,15 @@ public class LookupEdit : ComboBox
         }
     }
 
-
-
-
+    private void ResetAllItemVisible(object? sender, EventArgs e)
+    {
+        this.searchBox.Clear();
+        for (int index = 0; index < this.ItemCount; index++)
+        {
+            ComboBoxItem? item = this.ContainerFromIndex(index) as ComboBoxItem;
+            System.Diagnostics.Debug.Assert(item != null);
+            item.IsVisible = true;
+        }
+    }
 
 }
